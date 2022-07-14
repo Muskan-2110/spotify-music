c61c7ea8c75d5cd02cc4177b20339722d26c1ff7
Muskan Rani <68415889+Muskan-2110@users.noreply.github.com>
2022-07-14 10:48:47 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# spotify-music
+ This is My first major project using NextJs, tailwind CSS, middleware of NextJS.
---
+++ atoms/playlistAtom.js
@@ -0,0 +1,11 @@
+import { atom } from "recoil";
+
+export const playlistState = atom({
+    key: "playlistState",
+    default: null,
+});
+
+export const playlistIdState = atom({
+    key: "playlistIdState",
+    default: "4MnWe2YG7mCv83sxzzJDVf",
+});
